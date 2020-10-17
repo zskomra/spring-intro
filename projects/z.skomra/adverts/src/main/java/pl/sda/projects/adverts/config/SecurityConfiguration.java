6a0d0f35d103df7fa4f54f44a9e41e3f05203bcc
Zbigniew <z.skomraa@gmail.com>
2020-10-17 16:46:03 +0200

    acces to h2-console from website, changes in security and properties

--- projects/z.skomra/adverts/src/main/java/pl/sda/projects/adverts/config/SecurityConfiguration.java
+++ projects/z.skomra/adverts/src/main/java/pl/sda/projects/adverts/config/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -26,15 +27,17 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/register").permitAll()
-                .antMatchers("/login").anonymous()
+                .antMatchers("/login").permitAll()
+//                .antMatchers("/login").anonymous() - /todo do formularza logowania
                 .anyRequest().authenticated()
                 .and()
             .formLogin()
-                .loginPage("/login")
-                .defaultSuccessUrl("/index.html")
+//                .loginPage("/login") //todo do formularza logowania
+//                .failureUrl("/login-error") //todo do formularza logowania
+                .defaultSuccessUrl("/")
                 .and()
            .logout()
-                .logoutSuccessUrl("/index.html");
+                .logoutSuccessUrl("/login");
 
     }
 
@@ -52,4 +55,19 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .authoritiesByUsernameQuery("SELECT username, 'ROLE_USER' FROM users WHERE username = ?" )  ;
 
     }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .antMatchers("h2-console","/h2-console/**");
+    }
+
+    //    @Autowired
+//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//
+//        auth.inMemoryAuthentication()
+//                .withUser("user").password("password").roles("USER")
+//                .and()
+//                .withUser("admin").password("password").roles("ADMIN");
+//    }
 }
--- projects/z.skomra/adverts/src/main/java/pl/sda/projects/adverts/controllers/LoginController.java
+++ projects/z.skomra/adverts/src/main/java/pl/sda/projects/adverts/controllers/LoginController.java
@@ -1,22 +1,38 @@
-package pl.sda.projects.adverts.controllers;
-
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller @Slf4j
-@RequestMapping("/login")
-public class LoginController {
-
-    @GetMapping
-    public String prepareLoginPage () {
-        return "/login/log";
-    }
-
-    @PostMapping
-    public String processLoginPage () {
-        return "";
-    }
-}
+//package pl.sda.projects.adverts.controllers;
+//
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.ModelAttribute;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import pl.sda.projects.adverts.model.domain.User;
+//
+//@Controller
+//@Slf4j
+//@RequestMapping
+//public class LoginController {
+//
+//
+//    @GetMapping("/login")
+//    public String prepareLoginPage () {
+//        return "/login/login";
+//    }
+//
+////    @PostMapping
+////    public String loginError (Model model) {
+////        model.addAttribute("loginError",true);
+////        return "registration/registration-summary";
+////    }
+//
+//    @PostMapping
+//    public String processLoginPage (@ModelAttribute(name="user") User user, Model model) {
+//        String username = user.getUsername();
+//        String password = user.getPassword();
+//        if (username.equals("user")& password.equals("user"))
+//        return "registration/registration-summary";
+//        model.addAttribute("invalidCredentials", true);
+//        return "registration/registration-summary";
+//    }
+//}
